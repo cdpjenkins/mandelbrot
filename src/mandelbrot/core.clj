e2cec78fe708fb6184f1b504fab1a40257ca082d
Tom Sanders <tom.sanders@inqwell.com>
2019-12-07 21:24:19 +0000

    Render MB using Swing

--- src/mandelbrot/core.clj
+++ src/mandelbrot/core.clj
@@ -1,13 +1,10 @@
-(ns mandelbrot.core)
-
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(ns mandelbrot.core
+  (:require [mandelbrot.draw :as d]))
 
 
 (def max-iterations 255)
 (def step 0.002)
+(def bound 4)
 
 (defn square-complex [[x y]]
   [(- (* x x) (* y y))  (* 2 x y) ])
@@ -16,6 +13,7 @@
   [(+ x1 x2) (+ y1 y2)])
 
 (defn mandelbrot-step [x c]
+  "Return the next term in the Mandelbrot series"
   (let [xn+1 (add-complex (square-complex x) c)]
     xn+1))
 
@@ -26,7 +24,7 @@
 (defn within-bounds [z]
   (< (magnitude-squared z) 4))
 
-(defn mandelbot [c]
+(defn mandelbrot [c]
   (let [iterations (count
           (take max-iterations
                 (take-while within-bounds
@@ -36,20 +34,50 @@
 (defn text-mandelbrot []
   (doseq [y (range -1 1 step)]
     (doseq  [x (range -2 1 step)] 
-             (let [iterations (mandelbot [x y])]
+             (let [iterations (mandelbrot [x y])]
                (if (< iterations 99)
                  (print ".")
                  (print "X"))))
     (println)))
 
+(defn- mandelbrot-set
+  "Evaluate whether the imaginary numbers in the given range lie
+  within the Mandelbrot set "
+  [[x-start x-end x-step]
+   [y-start y-end y-step]
+   f]
+  (doseq [[y-idx y] (map-indexed #(vector %1 %2) (range y-start y-end y-step))]
+    (doseq [[x-idx x] (map-indexed #(vector %1 %2) (range x-start x-end x-step))]
+      (let [iterations (mandelbrot [x y])]
+        (f iterations x-idx y-idx)))))
+
+(defn- steps-count
+  [[start end step]]
+  (-> end
+      (- start)
+      (/ step)
+      ;(inc)
+      (int)))
+
+(defn gui-mandelbrot
+  []
+  (let [y [-1 1 step]
+        x [-2 1 step]
+        width (steps-count x)
+        height (steps-count y)]
+    (d/init-draw width height)
+    (mandelbrot-set x y d/mandelbrot-pixel)))
+
 (defn mandelbrot-seq [minx miny stepx stepy maxx maxy]
   (for [y (range miny maxy stepy)]
-    (for  [x (range minx maxx stepx)] 
-             (let [iterations (mandelbot [x y])]
+    (for  [x (range minx maxx stepx)]
+             (let [iterations (mandelbrot [x y])]
                iterations))
     ))
 
 
 (defn -main [& args]
-  (text-mandelbrot))
+  ;(text-mandelbrot)
+  (gui-mandelbrot)
+  )
 
---
+++ src/mandelbrot/draw.clj
@@ -0,0 +1,66 @@
+(ns mandelbrot.draw
+  (:import (java.util Arrays)
+           (javax.swing JFrame
+                        JPanel)
+           (java.awt Dimension
+                     Color)))
+
+(def backing-array (atom nil))
+(def panel (atom nil))
+(def colours-used (atom {}))
+
+(defn init-draw [width height]
+  (reset! backing-array (object-array (* width height)))
+
+  (let [f (JFrame. "Mandelbrot")
+        p (proxy [JPanel] []
+            (paintComponent [g]
+              ;(println "paint")
+              ; assumes array is filled in order, stops at first nil element
+              (proxy-super paintComponent g)
+              (loop [idx 0
+                     arr @backing-array
+                     len (alength arr)]
+                (when (< idx len)
+                  (let [pixel (get arr idx)
+                        x (quot idx height)
+                        y (mod idx height)]
+                    (when pixel
+                      (do
+                        (.setColor g pixel)
+                        (.drawLine g x y x y))))
+                  (recur (inc idx)
+                         arr
+                         len)))))]
+    (doto p
+      (.setBackground Color/BLACK)
+      (.setPreferredSize (Dimension. width height))
+      (.setOpaque true))
+    (doto f
+      (.setDefaultCloseOperation javax.swing.JFrame/EXIT_ON_CLOSE)
+      (.setContentPane p)
+      (.setVisible true)
+      (.pack)
+      (.show))
+    (reset! panel p)))
+
+(defn set-pixel!
+  "Set the pixel at x y to the given integer rgb value colour"
+  [x y rgb]
+  (let [c (or (get @colours-used rgb)
+              (get (swap! colours-used assoc rgb (Color. rgb)) rgb))
+        width (.getWidth @panel)
+        height (.getHeight @panel)
+        idx (+ (* x height) y)]
+    (aset @backing-array
+          idx
+          c)
+    (when (= x 0) ; repaint whole lines
+      (.repaint @panel 0 y width 1))))
+
+(defn mandelbrot-pixel
+  [iterations x y]
+  (let [color (if (< iterations 99)
+                0xFFFFFF
+                0x0)]
+    (set-pixel! x y color)))
